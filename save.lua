1846268997ddd7b2a384a8feef142353a6a0e6a3
cunly1 <140312899+cunly1@users.noreply.github.com>
2023-07-23 06:05:38 -0400

    Update save.lua

--- save.lua
+++ save.lua
@@ -1,1 +1,58 @@
-print("hi")
+local httpService = game:GetService('HttpService')
+local rawJSON = httpService:GetAsync("https://raw.githubusercontent.com/cunly1/test/main/properties.json")
+local decoded = httpService:JSONDecode(rawJSON)
+
+local result = httpService:JSONEncode(saveInstanceTree(...))
+
+function saveInstanceTree(instance)
+	local className = instance.ClassName
+	local classData = decoded[className]
+
+	assert(classData, "Couldn't find class with ClassName: ".. className)
+
+	local savedProperties = {}
+	local visitedClasses = {}
+
+	gatherProperties(instance, classData, savedProperties, visitedClasses)
+
+	local savedChildren = {}
+
+	for i, child in ipairs(instance:GetChildren()) do
+		savedChildren[i] = saveInstanceTree(child)
+	end
+
+	return {
+		ClassName = className,
+		Properties = savedProperties,
+		Children = savedChildren
+	}
+end
+
+function gatherProperties(instance, classData, savedProperties, visitedClasses)
+	if visitedClasses[classData] then return end
+	visitedClasses[classData] = true
+
+	for _, property in ipairs(classData.Properties) do
+		savedProperties[property] = getPropertyValue(instance, property)
+	end
+
+	if classData.Inherits then
+		for _, inheritedClass in ipairs(classData.Inherits) do
+			gatherProperties(instance, decoded[inheritedClass], savedProperties, visitedClasses)
+		end
+	end
+end
+
+function getPropertyValue(instance, propertyName)
+	local success, value = pcall(function() 
+		return instance[propertyName] 
+	end)
+
+	if success then 
+		return value 
+	end
+end
+
+print(result)
+
+return result
